4ef8edf27f3fea0def682e7be220f3347f54380d
Souhaila <souhailairzi@gmail.com>
2024-01-31 20:35:37 +0100

    hi

--- provar/Program.cs
+++ provar/Program.cs
@@ -3,5 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello montilivi");
+        Console.WriteLine("i'm learning Githup");
+        Console.WriteLine("Hello montilivi");
     }
 }
